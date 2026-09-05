5bbd69bb2498d3a540e846b6fbeaecb6c961819b
sujit kumar verma <sujitkumarvarma10@gmail.com>
2026-09-05 15:27:55 +0530

    bookallocation problem

--- practice-2.cpp
+++ practice-2.cpp
@@ -1271,3 +1271,139 @@
 //     cout<<peakIndexMountainArr(nums);
 //     return 0;
 // }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int singleElement(vector<int> nums){
+//     int n=nums.size();
+//     int st=0, end=n-1;
+//     if(n==1) return nums[0];
+//     while(st<=end){
+//         int mid=st+(end-st)/2;
+//         if(mid==0 && nums[0]!=nums[1]) return nums[mid];
+//         if(mid==n-1 && nums[n-1]!=nums[n-2]) return nums[mid];
+
+//         if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]) return nums[mid];
+//         if(mid%2==0){
+//             if(nums[mid-1]==nums[mid]){
+//                 end=mid-1;
+//             }
+//             else{
+//                 st=mid+1;
+//             }
+//         }
+//         else{
+//             if(nums[mid-1]==nums[mid]){
+//                 st=mid+1;
+//             }else{
+//                 end=mid-1;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+// int main(){
+//     vector<int> nums={1,1,2,3,3,4,4,8,8};
+//     // vector<int> nums={3,3,7,7,10,11,11};
+//     cout<<singleElement(nums);
+//     return 0;
+// }
+
+// // WAF to solve book allocation problem.arr={2,1,3,4},n=4,m=2;
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// bool isValied(vector<int> arr, int n, int m, int maxAllocatedPages){
+//     int student=1, pages=0;
+//     for(int i=0; i<n; i++){
+//         if(arr[i]>maxAllocatedPages) return false;
+//         if(pages+arr[i] <= maxAllocatedPages){
+//             pages+=arr[i];
+//         }
+//         else{
+//             student++;
+//             pages=arr[i];
+//         }
+//     }
+//     return student>m ? false : true;
+// }
+
+// int allocateBook(vector<int> arr, int n, int m){
+//     if(m>n) return -1;
+//     int sum=0; 
+//     for(int i=0; i<n; i++){
+//         sum+=arr[i];
+//     }
+
+//     int st=0,end=sum,ans=-1;
+//     while(st<=end){
+//         int mid=st+(end-st)/2;
+//         if(isValied(arr,n,m,mid)){
+//             ans=mid;
+//             end=mid-1;
+//         }
+//         else{
+//             st=mid+1;
+//         }
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     vector<int> arr={15,17,20};
+//     int n=3, m=2;
+//     cout<<allocateBook(arr,n,m);
+//     return 0;
+// }
+
+// //WAF to solve painter's partition problem. arr={40,30,10,20}, n=4, m=2.
+// #include<iostream>
+// #include<vector>
+// #include<climits>
+// using namespace std;
+
+// bool isPossible(vector<int> arr, int n, int m, int maxAllocatedTime){
+//     int painter = 1, time=0;
+//     for(int i=0; i<n; i++){
+//         if(time+arr[i] <= maxAllocatedTime){
+//             time+=arr[i];
+//         }
+//         else{
+//             painter++;
+//             time=arr[i];
+//         }
+//     }
+//     return painter<=m;
+// }
+
+// int minTimeToPainter(vector<int> arr, int n, int m){
+//     int maxVal=INT_MIN;
+//     int sum=0;
+//     for(int i=0; i<n; i++){
+//         sum+=arr[i];
+//         maxVal=max(maxVal,arr[i]);
+//     }
+//     int ans=-1;
+//     int st=maxVal, end=sum, mid;
+//     while(st<=end){
+//         mid=st+(end-st)/2;
+//         if(isPossible(arr,n,m,mid)){
+//             ans=mid;
+//             end=mid-1;
+//         }
+//         else{
+//             st=mid+1;
+//         }
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     vector<int> arr={40,30,10,20};
+//     int n=4, m=2;
+//     cout<<minTimeToPainter(arr,n,m);
+//     return 0;
+// }
